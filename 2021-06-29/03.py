4a8d0d70011f0b3b7dafcacbed5180114a68b576
Minyeob Kim <kanekoro812@gmail.com>
2021-06-30 09:19:28 +0000

    .

--- 2021-06-29/03.py
+++ 2021-06-29/03.py
@@ -1,11 +1,11 @@
-hobbies = [ 'reading', 'musig', 'movie' ]
+hobbies = ['reading', 'musig', 'movie']
 print(type(hobbies))
 
-hobbies = [ 100, 3.141592, 'reading' ]
+hobbies = [100, 3.141592, 'reading']
 print(hobbies)
 print()
 
-animals = [ 'lion', 'tiger', 'rabbit' ]
+animals = ['lion', 'tiger', 'rabbit']
 print(animals[0])
 print(animals[1])
 print(animals[2])
@@ -13,10 +13,7 @@ print()
 
 print(animals[1:])
 print(animals[0:len(animals)])
-print(animals[-1]) # 맨 마지막
+print(animals[-1])    # 맨 마지막
 print(animals[-3])
-print(animals[0:-1]) # 맨 마지막 전까지
+print(animals[0:-1])    # 맨 마지막 전까지
 print()
-
-
-
