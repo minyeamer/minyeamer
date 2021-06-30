d37cf2132fe9b73cdbc560cd0611a484b9a85c50
Minyeob Kim <kanekoro812@gmail.com>
2021-06-30 10:19:02 +0000

    ./03.py

--- 2021-06-29/03.py
+++ 2021-06-29/03.py
@@ -17,3 +17,38 @@ print(animals[-1])    # 맨 마지막
 print(animals[-3])
 print(animals[0:-1])    # 맨 마지막 전까지
 print()
+
+animals[1] = 'cat'
+print(animals)
+animals.append('dog')
+print(animals)
+del animals[1]
+print(animals)
+animals.insert(2, 'pig')
+print(animals)
+print()
+
+animals.append(3)
+animals.append('3.14')
+print(animals)
+print(type(animals[4]))
+print(type(animals[5]))
+print()
+
+del animals[3:]
+print(animals)
+print()
+
+print(animals.pop(2))
+print(animals)
+animals.append('pig')
+print()
+
+animals.append('lion')
+print(animals)
+animals.remove('lion')    # 1번째 요소만 삭제
+print(animals)
+print()
+
+animals.clear()
+print(animals)
